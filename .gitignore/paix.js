c642711093e79760065d9b4987ac62e9c22660d0
Ayoubssj <59485800+Ayoubssj@users.noreply.github.com>
2020-01-03 22:16:00 +0100

    Create paix.js

---
+++ .gitignore/paix.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+client.login(process.env.TOKEN);
+
+client.on("message", (message) => {
+    if (message.content.toLocaleLowerCase().includes("ping")) {
+      client.users.get("243774074432520192").send("Dédicace arouf gangsta");
+    } else
+   
+    if (message.content.startsWith("foo")) {
+      message.channel.send("bar!");
+    }
+  });
