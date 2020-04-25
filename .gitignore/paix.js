e39b3ad38e12dc2b0b38e783019d984814ce22e1
Ayoubssj <59485800+Ayoubssj@users.noreply.github.com>
2020-04-25 12:38:31 +0200

    Update paix.js

--- .gitignore/paix.js
+++ .gitignore/paix.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-client.login("NjYyMzM5MzQ2MzA0NTk4MDI2.Xhm6WA.vpInyrmVnI1RRmDWEQf6x3228lM");
+client.login(process.env.TOKEN);
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
 const adapter = new FileSync('database.json');
